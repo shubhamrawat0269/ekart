74d5cc8f54ec65c2fbe3d63e44b1fd9fdbb3a3bb
shubham Rawat <rawatshubam269@gmail.com>
2024-05-15 12:11:22 +0530

    Getting admin detail on Admin Dashboard

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -4,6 +4,7 @@ import OrderDetail from "../../components/admin/OrderDetail";
 import UserDetail from "../../components/admin/UserDetail";
 
 const AdminDashboard = () => {
+  const user = JSON.parse(localStorage.getItem("users"));
   return (
     <div>
       {/* Top */}
@@ -30,10 +31,23 @@ const AdminDashboard = () => {
             {/* text  */}
             <div className="">
               <h1 className=" text-center text-lg text-pink-500">
-                <span className=" font-bold">Name :</span> Kamal Nayan Upadhyay
+                <span className=" font-bold">Name : </span>
+                {user?.name}
               </h1>
+              {/* Email  */}
               <h1 className=" text-center text-lg text-pink-500">
-                <span className=" font-bold">Email :</span> test@gmail.com
+                <span className=" font-bold">Email : </span>
+                {user?.email}
+              </h1>
+              {/* Date  */}
+              <h1 className=" text-center text-lg text-pink-500">
+                <span className=" font-bold">Date : </span>
+                {user?.date}
+              </h1>
+              {/* Role  */}
+              <h1 className=" text-center text-lg text-pink-500">
+                <span className=" font-bold">Role : </span>
+                {user?.role}
               </h1>
             </div>
           </div>
